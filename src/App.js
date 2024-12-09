ee02b3921e1ebaf65b2a1c6f7c552a1a251449f1
vamsikrishnayl <157011885+vamsikrishnayl@users.noreply.github.com>
2024-12-09 22:10:02 +0530

    create react app and tailwin setup

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="text-3xl font-bold line-through">
+        Hello Vamsi All the best you are rocking
+      </div>
+      <span></span>
+    </div>
+  );
+}
+
+export default App;
